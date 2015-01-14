f1b484f2a635855a546162b86d40393625bc6e2c
Ken Wilcox <wilcoxk@gmail.com>
2015-01-14 16:53:17 -0700

    implements Serializable

--- app/src/main/java/com/k3nx/signupform/User.java
+++ app/src/main/java/com/k3nx/signupform/User.java
@@ -1,10 +1,12 @@
 package com.k3nx.signupform;
 
+import java.io.Serializable;
+
 /**
  * Created by Ken Wilcox on 1/14/2015 3:37 PM.
  * Simple user class
  */
-public class User {
+public class User implements Serializable {
     private String mUsername;
     private String mPassword;
 
